0050a39319cd7bfc801241398d96c1d9546c525c
바키찬 <ckstmznf@naver.com>
2025-05-12 20:14:48 +0900

    Feat: Input 컴포넌트 파일 분리

--- src/App.tsx
+++ src/App.tsx
@@ -2,52 +2,8 @@ import {GlobalStyle} from "./GlobalStyle";
 import {ThemeProvider} from "styled-components";
 import {Theme} from "./Theme";
 import {Button} from "./Button";
-
-import styled from "styled-components";
-import {FC, useState} from "react";
-
-export type InputProps = {
-    value: string;
-    onChange: (string) => void;
-    label: string;
-    placeholder: string;
-}
-
-const InputStyle = styled.div`
-
-`
-
-const LabelWrap = styled.div`
-  font-size: 18px;
-  font-weight: 400;
-`
-
-const InputContent = styled.input`
-  width: 100%;
-  font-size: 20px;
-  font-weight: 400;
-
-  padding: 12px;
-  margin-top: 15px;
-
-  outline: none;
-  background: none;
-  border: none;
-  border-bottom: 1px solid #A0A0A0;
-`
-
-export const Input: FC<InputProps> = ({value, onChange, placeholder, label}) => {
-    return (
-        <InputStyle>
-            <LabelWrap>{label}</LabelWrap>
-            <InputContent
-                value={value}
-                placeholder={placeholder}
-                onChange={(e) => onChange(e.target.value)}
-            />
-        </InputStyle>
-    );
-};
+import {useState} from "react";
+import {Input} from "./Input";
 
 
 function App() {
