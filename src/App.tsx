05a50da818389572915e7905d0ded2c004702c5c
바키찬 <ckstmznf@naver.com>
2025-05-12 13:19:19 +0900

    Feat: Initial Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import {GlobalStyle} from "./GlobalStyle";
+import {ThemeProvider} from "styled-components";
+import {Theme} from "./Theme";
 
 function App() {
     return (
         <div className="App">
+          <ThemeProvider theme={Theme}>
             <GlobalStyle/>
             Hello World
+          </ThemeProvider>
         </div>
     );
 }
