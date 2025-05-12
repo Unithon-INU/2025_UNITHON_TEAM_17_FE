cf2732d5fc43699f2bdb277a4d514b6eef1a192f
바키찬 <ckstmznf@naver.com>
2025-05-12 23:19:39 +0900

    Feat: PageLayout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,13 @@
 import {FC} from "react";
 import {RoutePath} from "../App";
 import {Link} from "react-router-dom";
+import {PageLayout} from "../styles/PageLayout";
 
 export const HomePage: FC = () => {
     return (
-        <div>
+        <PageLayout>
             HomePage
             <Link to={RoutePath.temp}>Temp Page로 이동</Link>
-        </div>
+        </PageLayout>
     )
 }
---
+++ src/styles/PageLayout.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const PageLayout = styled.div`
+  width: 100%;
+  height: 100vh;
+  max-width: 800px;
+  margin: 0 auto;
+`
