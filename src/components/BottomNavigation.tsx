64c1a3264e93dda732c807a21d9e50c6639a5f8b
바키찬 <ckstmznf@naver.com>
2025-05-12 23:54:04 +0900

    Feat: BottomNavigation Icon

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -11,24 +11,53 @@ const BottomNavigationStyle = styled.div`
 
   background: #fff;
   box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
-
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-
+  
   position: fixed;
   left: 0;
   bottom: 0px;
 `
 
+const ItonWrap = styled.div`
+  width: 100%;
+  max-width: 500px;
+  
+  margin: 0 auto;
+  
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  
+  position: relative;
+`
+
+const MainIcon = styled.div`
+  width: 76px;
+  height: 76px;
+  background-color: #999;
+  border-radius: 100%;
+  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -70%);
+`
+
 export const BottomNavigation: FC<BottomNavigationProps> = () => {
     return (
+
         <BottomNavigationStyle>
-            <Link to={RoutePath.chat}>채팅</Link>
-            <Link to={RoutePath.favorites}>즐겨찾기</Link>
-            <Link to={RoutePath.main}>홈</Link>
-            <Link to={RoutePath.warehouse}>창고</Link>
-            <Link to={RoutePath.my}>마이</Link>
+            <ItonWrap>
+                <Link to={RoutePath.chat}>채팅</Link>
+                <Link to={RoutePath.favorites}>즐겨찾기</Link>
+                <Link to={RoutePath.main}>
+                    <MainIcon>d</MainIcon>
+                </Link>
+                <Link to={RoutePath.warehouse}>창고</Link>
+                <Link to={RoutePath.my}>마이</Link>
+            </ItonWrap>
         </BottomNavigationStyle>
     );
 };
