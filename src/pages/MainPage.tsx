8cb6cd1aca110e0dd0b6cf54a8e511ab9e281626
바키찬 <ckstmznf@naver.com>
2025-05-19 19:04:24 +0900

    Feat: MainPage 게시물 type tab 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,12 @@
 import type {FC} from "react";
+import {useState} from "react";
 import {PageBackground, PageLayout} from "../styles/PageLayout";
 import {BottomNavigation} from "../components/BottomNavigation";
 import {MainHeader} from "../components/main/MainHeader";
 import {mockOfferings} from "../mocks/mockData";
 import {OfferingItem} from "../components/main/OfferingItem";
 import styled from "styled-components";
+import {OfferingTypeTab} from "../components/main/OfferingTypeTab";
 
 const OfferingList = styled.ul`
   display: flex;
@@ -13,11 +15,18 @@ const OfferingList = styled.ul`
   gap: 25px;
 `
 
+
 export const MainPage: FC = () => {
+    const all = "현재올라온할인"
+    const [selectedType, setSelectedType] = useState<string>(all);
+    // types에 중복제거 필요
+    const types = [all, ...new Set(mockOfferings.map((item => item.type)))]
+
     return (
         <PageBackground>
             <MainHeader/>
             <PageLayout>
+                <OfferingTypeTab types={types} onSelectType={(it) => setSelectedType(it)} selectedType={selectedType}/>
                 <OfferingList>
                     {mockOfferings.map((item) => (
                         <OfferingItem key={item.id} offering={item}/>
