fbfa4869c5df1d9b58ec9badbbea83157891ceeb
바키찬 <ckstmznf@naver.com>
2025-06-23 00:10:07 +0900

    Feat: ItemAddFunnel 기본 구조

--- src/pages/ItemAddPage.tsx
+++ src/pages/ItemAddPage.tsx
@@ -7,16 +7,73 @@ import {Location} from "../type/Warehouse";
 import {usePreviewImage} from "../hooks/UsePreviewImage";
 import {useLocation} from "react-router-dom";
 
+type ItemAddStep = "mascot" | "barcode" | "expireDate" | "edit" | "success";
+
+type ItemAddStepProps = {
+    onNext: (data: any | null) => void;
+}
+
+const ItemAddStepMascot: FC<ItemAddStepProps> = ({onNext}) => {
+    return (
+        <div>
+            <h2>아이템 추가</h2>
+            <p>아이템을 추가하려면 바코드와 유통기한 사진을 찍어주세요.</p>
+            <Button onClick={() => onNext(null)}>바코드 촬영하기</Button>
+        </div>
+    );
+}
+
+const ItemAddStepBarcode: FC<ItemAddStepProps> = ({onNext}) => {
+    return (
+        <div>
+            <h2>바코드 찍기</h2>
+            <p>아이템을 추가하려면 바코드와 유통기한 사진을 찍어주세요.</p>
+            <Button onClick={() => onNext(null)}>업로드</Button>
+        </div>
+    );
+}
+
+const ItemAddStepExpiredata: FC<ItemAddStepProps> = ({onNext}) => {
+    return (
+        <div>
+            <h2>유통기한 찍기</h2>
+            <p>아이템을 추가하려면 바코드와 유통기한 사진을 찍어주세요.</p>
+            <Button onClick={() => onNext(null)}>업로드</Button>
+        </div>
+    );
+}
+
+const ItemAddStepEdit: FC<ItemAddStepProps> = ({onNext}) => {
+    return (
+        <div>
+            <h2>수정</h2>
+            <p>아이템을 추가하려면 바코드와 유통기한 사진을 찍어주세요.</p>
+            <Button onClick={() => onNext(null)}>만들기</Button>
+        </div>
+    );
+}
+
+const ItemAddStepComplate: FC<ItemAddStepProps> = ({onNext}) => {
+    return (
+        <div>
+            <h2>완료</h2>
+            <p>아이템을 추가하려면 바코드와 유통기한 사진을 찍어주세요.</p>
+            <Button onClick={() => onNext(null)}>원래화면 돌아가기</Button>
+        </div>
+    );
+}
+
 export const ItemAddPage: FC = () => {
     const locationFunction = useLocation();
-    const { location } = locationFunction.state || {};
+    const {location} = locationFunction.state || {};
     const {shotBarcode, createItem, shotExpire} = useWarehouse();
     const {user} = useAuth()
 
+    const [step, setStep] = useState<ItemAddStep>("mascot");
     const [barcodeImage, onChangeBarcodeImage, barcodeImageUrl] = usePreviewImage(null);
     const [expireDateImage, onChangeExpireDateImage, expireDateImageUrl] = usePreviewImage(null);
-    const  barcode= useRef<BarcodeRes | null>(null);
-    const  expireDate= useRef<ExpireDateRes | null>(null);
+    const barcode = useRef<BarcodeRes | null>(null);
+    const expireDate = useRef<ExpireDateRes | null>(null);
 
     const onShotBarcode = async () => {
         if (!barcodeImage) return;
@@ -40,7 +97,6 @@ export const ItemAddPage: FC = () => {
 
         try {
             const res = await shotExpire(formData);
-            console.log(res)
             expireDate.current = res
         } catch (error) {
             console.error("Error uploading file:", error);
@@ -49,8 +105,8 @@ export const ItemAddPage: FC = () => {
 
     const onCreateItem = async () => {
         try {
-            const req : CreateItemReq = {
-                memberId : user!!.id,
+            const req: CreateItemReq = {
+                memberId: user!!.id,
                 locationId: location!!.id,
                 name: barcode.current!!.productName,
                 imageUrl: barcode.current!!.imageUrl,
@@ -65,33 +121,68 @@ export const ItemAddPage: FC = () => {
         }
     }
 
+    let stepTemplate
+    if (step === "mascot") {
+        stepTemplate = (
+            <ItemAddStepMascot
+                onNext={() => setStep("barcode")}
+            />
+        )
+    } else if (step === "barcode") {
+        stepTemplate = (
+            <ItemAddStepBarcode
+                onNext={() => setStep("expireDate")}
+            />
+        )
+    } else if (step === "expireDate") {
+        stepTemplate = (
+            <ItemAddStepExpiredata
+                onNext={() => setStep("edit")}
+            />
+        )
+    } else if (step === "edit") {
+        stepTemplate = (
+            <ItemAddStepEdit
+                onNext={onCreateItem}
+            />
+        )
+    } else if (step === "success") {
+        stepTemplate = (
+            <ItemAddStepComplate
+                onNext={() => setStep("mascot")}
+            />
+        )
+    }
+
     return (
         <PageBackground>
             <PageLayout>
-                <input type="file" accept="image/*" onChange={onChangeBarcodeImage} />
-                <input type="file" accept="image/*" onChange={onChangeExpireDateImage} />
-                {barcodeImageUrl && (
-                    <div style={{ marginTop: "10px" }}>
-                        <img
-                            src={barcodeImageUrl}
-                            alt="미리보기"
-                            style={{ maxWidth: "200px", border: "1px solid #ccc", borderRadius: "8px" }}
-                        />
-                    </div>
-                )}
+                {stepTemplate}
 
-                {expireDateImage && (
-                    <div style={{ marginTop: "10px" }}>
-                        <img
-                            src={expireDateImageUrl}
-                            alt="미리보기"
-                            style={{ maxWidth: "200px", border: "1px solid #ccc", borderRadius: "8px" }}
-                        />
-                    </div>
-                )}
-                <Button onClick={onShotBarcode}>추가</Button>
-                <Button onClick={onShotExpire}>유통기한</Button>
-                <Button onClick={onCreateItem}>저장</Button>
+                {/*<input type="file" accept="image/*" onChange={onChangeBarcodeImage}/>*/}
+                {/*<input type="file" accept="image/*" onChange={onChangeExpireDateImage}/>*/}
+                {/*{barcodeImageUrl && (*/}
+                {/*    <div style={{marginTop: "10px"}}>*/}
+                {/*        <img*/}
+                {/*            src={barcodeImageUrl}*/}
+                {/*            alt="미리보기"*/}
+                {/*            style={{maxWidth: "200px", border: "1px solid #ccc", borderRadius: "8px"}}*/}
+                {/*        />*/}
+                {/*    </div>*/}
+                {/*)}*/}
+
+                {/*{expireDateImage && (*/}
+                {/*    <div style={{marginTop: "10px"}}>*/}
+                {/*        <img*/}
+                {/*            src={expireDateImageUrl}*/}
+                {/*            alt="미리보기"*/}
+                {/*            style={{maxWidth: "200px", border: "1px solid #ccc", borderRadius: "8px"}}*/}
+                {/*        />*/}
+                {/*    </div>*/}
+                {/*)}*/}
+                {/*<Button onClick={onShotBarcode}>추가</Button>*/}
+                {/*<Button onClick={onShotExpire}>유통기한</Button>*/}
+                {/*<Button onClick={onCreateItem}>저장</Button>*/}
             </PageLayout>
         </PageBackground>
     );
