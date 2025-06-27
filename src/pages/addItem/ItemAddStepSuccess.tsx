2e2587abd4acdb4f7f5d294134ef07885918b594
imimimim43 <twintwin0243@naver.com>
2025-06-27 15:06:56 +0900

    feat : 창고 상품 등록 프로세스 ui 수정

--- src/pages/addItem/ItemAddStepSuccess.tsx
+++ src/pages/addItem/ItemAddStepSuccess.tsx
@@ -7,13 +7,15 @@ import {Space} from "../../components/common/Space";
 
 const Container = styled.div`
   min-height: 100%;
-  padding: 24px 32px;
-
+  padding: 24px 72px;
   display: flex;
   flex-direction: column;
+  align-items: center; 
+
 `
 
 const Title = styled.h2`
+  margin-top : 30px;
   font-size: 24px;
   font-weight: 600;
 `
@@ -25,27 +27,28 @@ const MascotImageWrap = styled.div`
 
 const Message = styled.p`
   color: #9d9d9d;
-  font-size: 16px;
+  font-size: 20px;
   font-weight: 500;
   text-align: center;
+  margin-bottom : 30px;
 `
 
 export const ItemAddStepSuccess: FC<ItemAddStepProps> = ({onNext}) => {
     return (
         <Container>
-            <Title>제품 등록 성공</Title>
+            <Title>상품 등록 성공 !</Title>
 
             <MascotImageWrap>
                 <img width={"100%"} src={mascotImage}/>
             </MascotImageWrap>
 
             <Message>
-                10점이 쌓였어요! <br/>
+                환경 점수 5점이 쌓였어요! <br/>
             </Message>
 
             <Space v={32}/>
 
-            <Button onClick={() => onNext(null)}>원래화면 돌아가기</Button>
+            <Button isFullWidth onClick={() => onNext(null)}>원래화면 돌아가기</Button>
         </Container>
     );
 }
