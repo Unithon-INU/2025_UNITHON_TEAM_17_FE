c19037f1e4aed7b50c1312f9ec1ae55896934c36
바키찬 <ckstmznf@naver.com>
2025-06-27 16:09:03 +0900

    Feat: 장소 수정 주석 제거

--- src/pages/Main/EditLocation.tsx
+++ src/pages/Main/EditLocation.tsx
@@ -12,57 +12,58 @@ import styled from "styled-components";
 
 const Form = styled.form`
   padding: 16px;
-  
+
   display: flex;
   flex-direction: column;
   gap: 16px;
 `;
 
 export const EditLocation: FC = () => {
-    // const navigate = useNavigate();
-    // const {id} = useParams()
-    // const {getLocation, updateLocation} = useWarehouse()
-    // const [location, setLocation] = useState<Location | null>(null);
+    const navigate = useNavigate();
+    const {id} = useParams()
+    const {getLocation, updateLocation} = useWarehouse()
+    const [location, setLocation] = useState<Location | null>(null);
 
-    // const onLoadLocation = async () => {
-    //     const foundLocation = await getLocation(Number(id));
-    //     setLocation(foundLocation);
-    // }
+    const onLoadLocation = async () => {
+        const foundLocation = await getLocation(Number(id));
+        setLocation(foundLocation);
+    }
 
-    // const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    //     e.preventDefault();
+    const onSubmit = async () => {
+        try {
+            const updatedLocations = await updateLocation(location!!.id, location!!);
+            navigate(RoutePath.warehouseDetail(id), {replace: true})
+        } catch (e) {
+            console.error("장소 수정 중 오류 발생:", e)
+        }
+    }
 
-    //     try {
-    //         const updatedLocations = await updateLocation(location!!.id, location!!);
-    //         navigate(RoutePath.warehouseDetail(id), {replace: true})
-    //     } catch (e) {
-    //         console.error("장소 수정 중 오류 발생:", e)
-    //     }
-    // }
+    useEffect(() => {
+        onLoadLocation()
+    }, [])
 
-    // useEffect(() => {
-    //     onLoadLocation()
-    // }, [])
-
-    // if (!location) {
-    //     return "장소를 불러오는 중입니다..."
-    // }
+    if (!location) {
+        return "장소를 불러오는 중입니다..."
+    }
 
     return (
         <PageBackground>
             <PageLayout>
-                장소수정
-                {/* <NavHeader title={"장소 수정"} backButton={true}/>
+                <NavHeader
+                    title={"장소 수정"}
+                    backButton={true}
+                    rightIcon={
+                        <div onClick={() => onSubmit()}>수정</div>
+                    }
+                />
 
-                <Form onSubmit={(e) => onSubmit(e)}>
+                <Form>
                     <InputRow
                         value={location.name}
                         onChange={v => setLocation({...location!!, name: v})}
                         label={"장소 이름"}
                     />
-
-                    <Button onClick={() => onSubmit(e)}>수정</Button>
-                </Form> */}
+                </Form>
 
             </PageLayout>
         </PageBackground>
